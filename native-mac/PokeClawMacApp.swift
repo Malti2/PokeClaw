fa729dd9e886827f022cbf17486375af704ce324
Malti2 <malte.hoepers@gmail.com>
2026-04-27 08:57:57 +0200

    Add menu bar extra and regular app activation

--- native-mac/PokeClawMacApp.swift
+++ native-mac/PokeClawMacApp.swift
@@ -1,10 +1,28 @@
+import AppKit
 import SwiftUI
 
 @main
 struct PokeClawMacApp: App {
+    init() {
+        NSApplication.shared.setActivationPolicy(.regular)
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+
+        MenuBarExtra("PokeClaw", systemImage: "pawprint") {
+            Button("Show PokeClaw") {
+                NSApp.activate(ignoringOtherApps: true)
+            }
+
+            Divider()
+
+            Button("Quit PokeClaw") {
+                NSApplication.shared.terminate(nil)
+            }
+        }
+        .menuBarExtraStyle(.window)
     }
 }
